971b34d7999d9574a40d681d20c2fa10f695f858
Christopher Burr <chrisdburr@gmail.com>
2025-02-28 21:03:05 +0000

    update CORS settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -123,6 +123,31 @@ CORS_ALLOWED_ORIGINS = [
 # Allow credentials (cookies, authorization headers)
 CORS_ALLOW_CREDENTIALS = True
 
+# CORS settings
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "http://localhost:3000").split(
+    ","
+)
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
 INTERNAL_IPS = [
     "127.0.0.1",
 ]

8aca61a4b11837a296eaad53a776202bbd39a8c1
Christopher Burr <chrisdburr@gmail.com>
2025-03-05 07:50:24 +0000

    update docker build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	reactStrictMode: true,
 	// No rewrites at all
+
+	// Add this output configuration for standalone mode
+	output: "standalone",
 };
 
 export default nextConfig;

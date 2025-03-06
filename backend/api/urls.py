22685866f4ea6cb56b836d6e0aa8e82e22f3742f
Christopher Burr <chrisdburr@gmail.com>
2025-03-06 21:41:55 +0000

    update backend to new techniques

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -12,23 +12,29 @@ from .views.api_views import (
     AttributeTypesViewSet,
     AttributeValuesViewSet,
     ResourceTypesViewSet,
-    TechniqueRelationshipsViewSet,
 )
 
 router = routers.DefaultRouter()
-router.register(r'assurancegoals', AssuranceGoalsViewSet)
-router.register(r'category', CategoryViewSet)
-router.register(r'subcategory', SubCategoryViewSet)
-router.register(r'tags', TagsViewSet)
-router.register(r'techniques', TechniquesViewSet)
-router.register(r'attributetypes', AttributeTypesViewSet)
-router.register(r'attributevalues', AttributeValuesViewSet)
-router.register(r'resourcetypes', ResourceTypesViewSet)
-router.register(r'techniquerelationships', TechniqueRelationshipsViewSet)
+router.register(r"assurancegoals", AssuranceGoalsViewSet)
+router.register(r"category", CategoryViewSet)
+router.register(r"subcategory", SubCategoryViewSet)
+router.register(r"tags", TagsViewSet)
+router.register(r"techniques", TechniquesViewSet)
+router.register(r"attributetypes", AttributeTypesViewSet)
+router.register(r"attributevalues", AttributeValuesViewSet)
+router.register(r"resourcetypes", ResourceTypesViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),  # API endpoints
-    path('get_categorylist/<int:assurance_goal_id>/', get_categorylist, name='get_categorylist'),
-    path('get_subcategorylist/<int:category_id>/', get_subcategorylist, name='get_subcategorylist'),
-    path('debug/', debug_endpoint, name='debug_endpoint'),
+    path("", include(router.urls)),  # API endpoints
+    path(
+        "get_categorylist/<int:assurance_goal_id>/",
+        get_categorylist,
+        name="get_categorylist",
+    ),
+    path(
+        "get_subcategorylist/<int:category_id>/",
+        get_subcategorylist,
+        name="get_subcategorylist",
+    ),
+    path("debug/", debug_endpoint, name="debug_endpoint"),
 ]
